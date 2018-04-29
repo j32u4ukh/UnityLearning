6f4687b35ee05ce9145f83d76ff97ad226d4f0c2
j32u4ukh <j32u4ukh@gmail.com>
2018-04-29 21:54:11 +0800

    add Unity project

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public GameObject player;   // 攝影機跟隨對象
+    private Vector3 offset;     // 攝影機和跟隨對象之間的距離
+
+    // Use this for initialization
+    void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.transform.position + offset;
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+    public float Speed;    
+    public AudioClip soundPickUp;
+    public Text countText, winText;
+    private Rigidbody rb;
+    private int count;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        winText.text = "";
+
+    }
+
+    // 計算物理性前即執行
+    void FixedUpdate(){
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * Speed);
+    }
+
+    private void OnTriggerEnter(Collider other){
+        if (other.gameObject.CompareTag("PickUp")) {
+            other.gameObject.SetActive(false);
+            AudioSource.PlayClipAtPoint(soundPickUp, transform.position);
+            count++;
+            setCountText();
+        }
+    }
+
+    void setCountText() {
+        countText.text = "Count: " + count.ToString();
+        if (count == 12){
+            winText.text = "You Win!";
+        }
+    }
+}
